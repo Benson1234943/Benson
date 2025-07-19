579c0569b0432046d388ae9957daf85499895ee3
Benson1234943 <Benson.Wambogo@gmail.com>
2025-07-19 15:09:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed (".text",
+    {
+        strings: ["Software Engineer","Cloud Architect","Cloud Engineer","Java Script Engineer","UI/UX Designer"],
+        backSpeed:100,
+        typedSpeed:100,
+        backDelay:100,
+        loop:true
+    }
+    
+)
+
